e621d107e75bdf9aacc7bac41796fb40c5419c29
MohamedHamed0007 <moh.hamed.elrefaey@gmail.com>
2026-05-16 20:43:17 +0300

    Add metrics collection using prom-client in login page

--- admin-next/app/login/page.tsx
+++ admin-next/app/login/page.tsx
@@ -1,10 +1,21 @@
 "use client";
-
+import client from "prom-client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { loginUser, clearError } from "@/store/authSlice";
 
+client.collectDefaultMetrics();
+
+export async function GET() {
+  const metrics = await client.register.metrics();
+
+  return new Response(metrics, {
+    headers: {
+      "Content-Type": client.register.contentType,
+    },
+  });
+}
 export default function LoginPage() {
   const router   = useRouter();
   const dispatch = useAppDispatch();
